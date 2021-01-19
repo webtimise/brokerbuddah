561c6470b7e470dc07be4fbf73ab0389fc0d2128
webtimise <hello@webtimise.fr>
2021-01-19 09:23:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -16,10 +16,10 @@ sections.forEach((section, i) => {
     onToggle: self => self.isActive && setSection(section),
 
     onEnter: () => {
-      gsap.from(".step-action", {y: 20, duration: 1})
+      gsap.from(".step-action", {y: 10, duration: 1, autoAlpha: 1})
       },
-    onLeaveBack: () => {
-      gsap.to(".step-action", {y: 0, duration: 1})
+    onLeave: () => {
+      gsap.to(".step-action", {y: 0, duration: 1, autoAlpha: 0})
     },
     markers: true
 
